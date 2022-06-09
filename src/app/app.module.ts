84d55a906c8eb0867f83778091f56879d80ed040
Jakub Lacný <jakub.lacny@messenger.cz>
2022-06-09 19:19:02 +0200

    development starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { ConcertsComponent } from './concerts/concerts.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    ConcertsComponent,
+    ShopComponent,
+    ContactsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
